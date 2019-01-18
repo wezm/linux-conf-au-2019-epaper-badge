577c73a25cf07943b2c490ea34357c749f28ff5d
Wesley Moore <wes@wezm.net>
2019-01-19 08:05:53 +1100

    Add method to save hi count

--- src/app.rs
+++ src/app.rs
@@ -1,6 +1,9 @@
 use nix::sys::utsname::UtsName;
 use std::collections::HashMap;
+use std::fs;
+use std::io;
 use std::net::IpAddr;
+use std::path::Path;
 use std::time::{Duration, Instant};
 use systemstat::{Ipv4Addr, Memory};
 
@@ -40,5 +43,20 @@ impl State {
 
     pub fn hi_count(&self) -> usize {
         self.hellos.len()
+    }
+
+    pub fn save_hi_count(&self, path: &Path) -> io::Result<()> {
+        let tmp_filename = path
+            .file_name()
+            .map(|file_name| {
+                let mut tmp_name = file_name.to_os_string();
+                tmp_name.push(".tmp");
+                tmp_name
+            })
+            .ok_or_else(|| io::Error::new(io::ErrorKind::Other, "file_name is None"))?;
+        let tmp_path = path.with_file_name(tmp_filename);
+        fs::write(&tmp_path, self.hi_count().to_string())?;
+
+        std::fs::rename(tmp_path, path)
     }
 }
